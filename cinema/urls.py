819333380c842ae1a44a476545e5d0980b0cd203
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-08-09 15:51:57 +0300

    Fix url for the genre details route (add slash in the end)

--- cinema/urls.py
+++ cinema/urls.py
@@ -17,7 +17,7 @@ router.register("movies", MovieViewSet)
 
 urlpatterns = [
     path("genres/", GenreList.as_view(), name="genre-list"),
-    path("genres/<int:pk>", GenreDetail.as_view(), name="genre-detail"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
     path("", include(router.urls)),
