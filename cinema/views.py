4f2c446d8973d860780cc1eb80935e922b0e6674
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-08-10 10:21:16 +0300

    Refactor serializer validation in views

--- cinema/views.py
+++ cinema/views.py
@@ -23,11 +23,9 @@ class GenreList(APIView):
     def post(self, request) -> Response:
         serializer = GenreSerializer(data=request.data)
 
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class GenreDetail(APIView):
@@ -40,20 +38,16 @@ class GenreDetail(APIView):
 
     def put(self, request, pk: int) -> Response:
         serializer = GenreSerializer(self.get_object(pk=pk), data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def patch(self, request, pk: int) -> Response:
         serializer = GenreSerializer(
             self.get_object(pk=pk), data=request.data, partial=True)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def delete(self, request, pk: int) -> Response:
         self.get_object(pk=pk).delete()
