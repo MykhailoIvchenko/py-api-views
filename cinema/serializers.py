eaf3ee5a5865f7499bc02c5dec26a1c3703e6d8f
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-08-10 12:18:32 +0300

    Delete redundant fields from serializers

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -4,41 +4,24 @@ from cinema.models import Movie, Genre, Actor, CinemaHall
 
 
 class GenreSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=100)
-
     class Meta:
         model = Genre
         fields = "__all__"
 
 
 class ActorSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-    first_name = serializers.CharField(max_length=100)
-    last_name = serializers.CharField(max_length=100)
-
     class Meta:
         model = Actor
         fields = "__all__"
 
 
 class CinemaHallSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=100)
-    rows = serializers.IntegerField(min_value=1)
-    seats_in_row = serializers.IntegerField(min_value=1)
-
     class Meta:
         model = CinemaHall
         fields = "__all__"
 
 
 class MovieSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=255)
-    description = serializers.CharField()
-    duration = serializers.IntegerField()
-
     class Meta:
         model = Movie
         fields = ["id", "title", "description", "duration"]
